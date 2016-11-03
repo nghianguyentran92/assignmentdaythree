513c4975ff08fc451108061514a1ca22df4153ae
nghianguyentran92 <nghianguyentran92@gmail.com>
2016-11-03 10:20:12 +0700

    assignment 3

--- exportExcel/exportexcel.js
+++ exportExcel/exportexcel.js
@@ -21,12 +21,12 @@ fetch(
 
 const transformExcel = data => {
 
-    // Create a new worksheet with 10 columns and 12 rows
+
     let sheet1 = workbook.createSheet('sheet1',data.dataElements.length,data.dataElements.length);
 
-    // Fill some data
+
     let count = 1;
-    let countsecond = 2;
+
 
     data.dataElements.forEach( result => {
           sheet1.set(1,count,result.id);
